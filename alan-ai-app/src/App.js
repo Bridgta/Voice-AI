699e661154d7184b8aa1363d020ceb2ca066b24b
Bridget Asser <bridgetasser@gmail.com>
2020-11-02 20:26:07 -0500

    updates to ui of news card, Alanai NewsAPI pull latest article header

--- alan-ai-app/src/App.js
+++ alan-ai-app/src/App.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
+import NewsCards from "./components/NewsCards/NewsCards";
+
+const alanKey =
+   ;
 
 const App = () => {
+    const [newsArticles, setNewsArticles] = useState([]);
+
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+            onCommand: ({ command, articles }) => {
+                if (command === "newHeadlines") {
+                    setNewsArticles(articles);
+                }
+            },
+        });
+    }, []);
+
     return (
         <div>
             <h1>Alan AI News Application</h1>
+            <NewsCards articles={newsArticles} />
         </div>
     );
 };
---
+++ alan-ai-app/src/components/NewsCard/NewsCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {
+    Crad,
+    CardActions,
+    CardActionArea,
+    CardContent,
+    cardMedia,
+    Button,
+    Typography,
+} from "@material-ui/core";
+
+const NewsCard = () => {
+    return <div>News Card</div>;
+};
+
+export default NewsCard;
